b2ef3178b4aeb49f5c3a95e37dd54807c5842c31
Moezzain <moezzain94@gmail.com>
2021-11-05 13:34:52 +0300

    Education and experience added

--- src/components/layouts/home03/education/TopEducation.jsx
+++ src/components/layouts/home03/education/TopEducation.jsx
@@ -9,54 +9,80 @@ class TopEducation extends Component {
                     id: 1,
                     classname: 'experience-box experience-box-line t3',
                     datawow: '0.3s',
-                    year: 'Universite of Udacity',
-                    title: 'User Experience Design',
-                    text: 'User experience (UX) design is the process design teams use to create products.'
+                    year: 'SUST 2012-2017',
+                    title: 'Sudan University of science and technology',
+                    text: ''
                 },
-                {
-                    id: 2,
-                    classname: 'experience-box experience-box-line t3',
-                    datawow: '0.5s',
-                    year: 'Udemy',
-                    title: 'User Centered Design',
-                    text: 'User experience (UX) design is the process design teams use to create products.'
-                },
-                {
-                    id: 3,
-                    classname: 'experience-box t3',
-                    datawow: '0.7s',
-                    year: 'Skil Share',
-                    title: 'Interaction Design',
-                    text: 'User experience (UX) design is the process design teams use to create products.'
-                }
+                // {
+                //     id: 2,
+                //     classname: 'experience-box experience-box-line t3',
+                //     datawow: '0.5s',
+                //     year: 'Udemy',
+                //     title: 'User Centered Design',
+                //     text: 'User experience (UX) design is the process design teams use to create products.'
+                // },
+                // {
+                //     id: 3,
+                //     classname: 'experience-box t3',
+                //     datawow: '0.7s',
+                //     year: 'Skil Share',
+                //     title: 'Interaction Design',
+                //     text: 'User experience (UX) design is the process design teams use to create products.'
+                // }
             ],
             contentbot : [
                 {
                     id: 1,
                     classname: 'experience-box experience-box-line t3',
                     datawow: '0.8s',
-                    year: '2013 - 2015',
-                    title: 'Designer at',
-                    office: 'Microsoft',
-                    text: 'User experience (UX) design is the process design teams use to create products.'
+                    year: '2018 - 2020',
+                    title: 'Software Developer at',
+                    office: 'AZ Technology',
+                    text: 'Developing apps specialized in banking services using languages like C, C++, Java.'
                 },
                 {
                     id: 2,
                     classname: 'experience-box experience-box-line t3',
                     datawow: '1s',
-                    year: '2013 - 2017',
-                    title: 'User Researcher at',
-                    office: 'Apple',
-                    text: 'User experience (UX) design is the process design teams use to create products.'
+                    year: '12/2018 – 12/2020',
+                    title: 'React Developer at',
+                    office: 'XZone',
+                    text: 'Part-time employee working for XZone which is a company focused on implementing E-Commerce applications for vendors'
                 },
                 {
                     id: 3,
                     classname: 'experience-box t3',
                     datawow: '1.2s',
-                    year: '2017 - 2020',
-                    title: 'Lead Designer at',
-                    office: 'Dripple',
-                    text: 'User experience (UX) design is the process design teams use to create products.'
+                    year: '12/2020 – 03/202',
+                    title: 'Full-stack Developer at',
+                    office: 'Orderpharma',
+                    text: 'As a full-time developer implementing Orderpharma dashboard and backend using MERN stack.'
+                },{
+                    id: 4,
+                    classname: 'experience-box experience-box-line t3',
+                    datawow: '0.8s',
+                    year: '2018 - 2020',
+                    title: 'Full stack Developer at',
+                    office: 'Utopia Tech PTY LTD',
+                    text: 'Part-time employee working in an Agile environment using technolgies such as Next.js, Node, AWS lambda, etc..'
+                },
+                {
+                    id: 5,
+                    classname: 'experience-box experience-box-line t3',
+                    datawow: '1s',
+                    year: '12/2018 – 12/2020',
+                    title: 'Part time React Developer at',
+                    office: 'Friyay Company',
+                    text: 'Part-time employee using technolgies such as React js, graphQl relay, Redux, froala editor, etc..'
+                },
+                {
+                    id: 6,
+                    classname: 'experience-box t3',
+                    datawow: '1.2s',
+                    year: '12/2020 – 03/202',
+                    title: 'Full-stack Developer at',
+                    office: 'My Ticket Company',
+                    text: 'Full-time employee using technologies such as React js, Node js, MongoDB, etc..'
                 }
             ]
         }
@@ -72,7 +98,7 @@ class TopEducation extends Component {
                                 <div className="col-lg-4 col-12 last-col animate-element wow delay5 fadeIn" data-wow-delay={data.datawow} key={data.id}>
                                     <div className={data.classname}>
                                         <div className="box-inner">
-                                        <h3 className="f-info bg-s1 color-d6">{data.year}</h3>
+                                        <h3 className="education-edit bg-s1 color-d6">{data.year}</h3>
                                         <div className="exp-wrap">
                                             <div className="s-info mg-b11">{data.title}</div>
                                             <p>
